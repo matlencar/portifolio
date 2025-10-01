54bb7b8952848f4ad3084dcd7e0528447374546c
Matheus Alencar Silva <87335529+matlencar@users.noreply.github.com>
2025-10-01 17:03:24 -0300

    Add files via upload
    Primeiro commit de pórtifolio

---
+++ src/app/app.ts
@@ -0,0 +1,10 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  standalone: true,
+  template: `<router-outlet />`
+})
+export class App { }
---
+++ src/app/module/portifolio/pages/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header';
+
+@Component({
+  selector: 'app-home',
+  imports: [HeaderComponent],
+  templateUrl: './home.html',
+  styleUrl: './home.scss'
+})
+export class HomeComponent {
+
+}
